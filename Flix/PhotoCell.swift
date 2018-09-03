ca68f4f952a2acc69c516f21455224d8e757eefd
Neil Shah <neilmshah01@gmail.com>
2018-09-03 15:39:53 -0700

    initial commit: done adding tableView

---
+++ Flix/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  Flix
+//
+//  Created by Neil Shah on 9/3/18.
+//  Copyright © 2018 Neil Shah. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
