d58a884d9dd8ab67a6c10749cb26f4f303be197d
Neil Shah <neilmshah01@gmail.com>
2018-09-05 09:55:23 -0700

    fixed row height

--- Flix/PhotosViewController.swift
+++ Flix/PhotosViewController.swift
@@ -20,6 +20,7 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.rowHeight = 250
 
         // Network request snippet
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
